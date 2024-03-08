169da7a81cca824c44288341b9becb51a73408ed
MrHallKellett <chall@kellettschool.com>
2024-03-08 20:12:53 +0800

    day 3 - 9min 24

--- day3.py
+++ day3.py
@@ -1,28 +1,44 @@
 from collections import defaultdict
 
 with open("day3.txt") as f:
-    ACTUAL_DATA = f.read().splitlines()
+    ACTUAL_DATA = f.read().splitlines()[0]
 
 
-TEST_DATA = ''''''.splitlines()
+TEST_DATA = '''12 6 4 1 9 5 5 1 9 1 7'''
 
-EXPECTED = 2533
+EXPECTED = "F"
 
 
 def solve(data):
+    global line_length
+    j = 0
+    for i, thing in enumerate(data.split()):
+        thing = int(thing)
+
+        
+        if i % 2 == 0:
+            char = "."
+        else:
+            char = "#"
+        for i in range(thing):
+            print(char, end="")
+            j += 1
+            if j == line_length:
+                print()
+                j = 0
 
    
 
-    print("Result obtained:", result)
-    return result
 
 
 
 
 if __name__ == "__main__":
-
-    assert solve(TEST_DATA) == EXPECTED
+    line_length = 10
+    solve(TEST_DATA) == EXPECTED
     print("TEST PASSED!!!")
+    line_length = 100
+    print()
 
     
     print(solve(ACTUAL_DATA))
